23a85562a554966522728dc06b37a66233c859d5
Anurag <61429664+JohnNash112@users.noreply.github.com>
2022-06-07 02:14:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Optional
+import pandas as pd
+import numpy as np
+from tsai.all import *
+from tsai.inference import load_learner
+
+
 app = FastAPI()
 
 class Cord(BaseModel):
@@ -29,7 +35,7 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return{"hello": "there!"}
+    return("hello there!")
 
 # collected_data = []    
 # @app.post("/analyse")
@@ -40,16 +46,25 @@ def read_root():
 #     df = pd.DataFrame(collected_data, columns =['timestamp', 'latitude', 'longitude', 'speed'])
 #     print(df)
 #     return dataRec
-
-# @app.get("/score")
-# async def getScore():
-#     readData()
-#     print(".........1st execution done..........")
-#     extractFeatures()
-#     print(".........2nd execution done..........")
-#     markEvents()
-#     print(".........3rd execution done..........")
-#     responseGot = getCDF()
-#     print(".........4th execution done..........")
-#     return [{"Harsh Acceleration": responseGot[0][0],"Harsh Braking": responseGot[0][1],"Harsh Turning": responseGot[0][2]}]
-# print(collectedData)
+@app.get("/score")
+def getScore():
+    dframe = pd.read_csv('/home/dbx/finalApp/download_bike_speed.csv', header=None)
+    dframe.columns = ["time","Latitude","Longitude","speed"]
+    dframe["TripID"] = 'T-0'
+    dframe["Latitude"] = pd.to_numeric(dframe["Latitude"], errors ='ignore')
+    dframe["Longitude"] = pd.to_numeric(dframe["Longitude"], errors ='ignore')
+    dframe["speed"] = pd.to_numeric(dframe["speed"], errors ='ignore')
+    dframe = dframe.sort_values(by=['time'], ascending=True)
+    dframe2 = np.zeros((1630-len(dframe), 5))
+    dframe2 = pd.DataFrame(dframe2, columns=["time","Latitude", "Longitude", "speed", "TripID"])
+    dframe2["TripID"] = 'T-0'
+    newFrame = [dframe, dframe2]
+    dframe = pd.concat(newFrame)
+    # getting test data
+    X_test, y_test = df2Xy(dframe, sample_col='TripID', sort_by ='time',data_cols=['Latitude','Longitude','speed'] ,steps_in_rows=True)
+    clf = load_learner("/home/dbx/finalApp/backend/trainedModel.pkl")
+    probas, target, preds = clf.get_X_preds(X_test)
+    if(preds=='[0.0]'):
+        return{"Class":"Safe"}
+    else:
+        return{"Class":"Unsafe"}
